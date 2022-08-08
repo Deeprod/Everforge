f228ab3d771c3d6f2f60a675582492be7b9b8ab5
Deeprod <106252325+Deeprod@users.noreply.github.com>
2022-08-08 23:40:13 +1000

    More assets, forge and birds

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,17 +8,21 @@ public class CameraMovement : MonoBehaviour
 
     [Header("Camera Settings")]
     public float CameraSpeed;
+    public float CameraMaxRight;
+    public float CameraMaxLeft;
 
     [Header("Cloud Background Lock")]
     public GameObject[] cloud;
 
     private float movX;
     private float movY;
+    private float cameraPositionStart;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraPosition = this.transform.position;
+        cameraPositionStart = cameraPosition.x;
     }
 
     // Update is called once per frame
@@ -29,22 +33,26 @@ public class CameraMovement : MonoBehaviour
 
         if(Input.GetKey(KeyCode.D))
         {
-            movX = Mathf.Cos((Mathf.PI/180)*26.565f)/10;
-            movY = -Mathf.Sin((Mathf.PI/180)*26.565f)/10;
+            movX = Mathf.Cos((Mathf.PI/180)*26.565f)/10 * CameraSpeed;
+            movY = -Mathf.Sin((Mathf.PI/180)*26.565f)/10 * CameraSpeed;
         }
         if(Input.GetKey(KeyCode.A))
         {
-            movX = -Mathf.Cos((Mathf.PI/180)*26.565f)/10;
-            movY = Mathf.Sin((Mathf.PI/180)*26.565f)/10;
+            movX = -Mathf.Cos((Mathf.PI/180)*26.565f)/10 * CameraSpeed;
+            movY = Mathf.Sin((Mathf.PI/180)*26.565f)/10 * CameraSpeed;
         }
 
         cameraPosition.x += movX;
         cameraPosition.y += movY;
-        for (int i = 0; i < cloud.Length; i++)
-        {
-            cloud[i].transform.position = new Vector3(cloud[i].transform.position.x + movX, cloud[i].transform.position.y + movY, cloud[i].transform.position.z);
-        }
-        this.transform.position = cameraPosition;
+
+        //if(cameraPosition.x < cameraPositionStart + CameraMaxRight || cameraPosition.x > cameraPositionStart - CameraMaxLeft)
+        //{
+            for (int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i].transform.position = new Vector3(cloud[i].transform.position.x + movX, cloud[i].transform.position.y + movY, cloud[i].transform.position.z);
+            }
+            this.transform.position = cameraPosition;
+        //}
 
         //for (int i = 0; i < cloud.Length; i++)
         //{
